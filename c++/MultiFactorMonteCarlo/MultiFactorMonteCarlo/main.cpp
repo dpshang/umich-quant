49c4023a3c21a8784a64df1c315d61376b896f5d
dpshang <Roc@0587390172.wireless.umich.net>
2017-05-22 21:35:45 -0400

    New Update

--- c++/MultiFactorMonteCarlo/MultiFactorMonteCarlo/main.cpp
+++ c++/MultiFactorMonteCarlo/MultiFactorMonteCarlo/main.cpp
@@ -33,10 +33,11 @@ int main(int argc, const char * argv[]) {
     const auto pApple = market.marketFactor("AAPL");
     std::cout << "AAPL variance = " << pApple->variance() << std::endl; //td::endl---flushes the output buffer
     
-    int numberHistoricalReturns = market.numberHistoricalReturns();
-    MarketSimulation simulation0(numberHistoricalReturns);
+    int numberHistoricalReturns = 252;//use one year
+    double ewmaLambda = 0.97;
+    MarketSimulation simulation0(numberHistoricalReturns, ewmaLambda);
     std::cout << "AAPL return = " << pApple->simulatedReturn(simulation0) << std::endl;
-    MarketSimulation simulation1(numberHistoricalReturns);
+    MarketSimulation simulation1(numberHistoricalReturns, ewmaLambda);
     
     Portfolio portfolio;
     portfolio.addPosition(std::make_shared<Security>(*market.marketFactor("AAPL")), 1000);
@@ -47,9 +48,19 @@ int main(int argc, const char * argv[]) {
     
     const MarketScenario scenario(market, 2016, 9, 30);
     size_t numSims = 9999;
-    std::vector<PortfolioSimResult> positionResults = portfolio.simResultsByPosition(scenario, numberHistoricalReturns,numSims);    
+    std::vector<PortfolioSimResult> positionResults = portfolio.simResultsByPosition(scenario, numberHistoricalReturns,numSims, ewmaLambda);
     PortfolioSimResult portfolioResults(positionResults);
-    std::cout << "95% VaR = " << portfolioResults.var(0.95)<<std::endl;
-    std::cout << "Expected Shortfall = " << portfolioResults.expectedShortfall(0.95) << std::endl;
+    double quantile = 0.025;
+    double percent = quantile * 100;
+    std::cout << percent << "% quantile VaR = " << portfolioResults.var(quantile)<<std::endl;
+    std::cout << percent << "% quantile ES = " << portfolioResults.expectedShortfall(quantile) << std::endl;
+    std::vector<int> portfolioVarEvents(portfolioResults.varEvents(quantile));
+    double totalES = 0;
+    for(auto result:positionResults){
+        double incrementalES = -result.averagePnLonEvents(portfolioVarEvents);
+        totalES += incrementalES;
+        std::cout << incrementalES << std::endl;
+    }
+    std::cout << "Sum of IES = " << totalES << std::endl;
     return 0;
 }
